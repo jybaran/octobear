5b68d24e96e286912943d6cacc41f138dab4c7a1
Jenny Baran <jenny.baran@gmail.com>
2014-11-07 11:58:07 -0500

    bootstrap & first run of pulling from 8tracks
    currently seems to redirect to 8track oops we’ll have to fix that

--- app.py
+++ app.py
@@ -3,6 +3,23 @@ import json, urllib2
 
 app = Flask(__name__)
 
-if __name__ = "__main__":
+@app.route("/")
+def index():
+    return "hello"
+
+@app.route("/t/<tag>")
+def tag(tag="manatee"):
+    url = "http://8tracks.com/mix_sets/tags:%s.json?include=mixes&api_key=b72f8beba38aa9fb230d52bf6f45e2baf9fbf322"
+    #add tags w/ +
+    #spaces to underscores
+    #alternately keyword:<SEARCH TERM>
+    url = url%(tag)
+    request = urllib2.urlopen(url)
+    resultstring = request.read()
+    #result = json.loads(resultstring)
+    #s = ""
+    return resultstring
+
+if __name__ == "__main__":
     app.debug = True
     app.run()
